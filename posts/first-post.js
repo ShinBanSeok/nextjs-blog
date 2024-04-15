3261a166f51ac631a6b6d9695383f95a5224ebe9
ShinBanSeok <iop000126@naver.com>
2024-04-15 14:07:21 +0900

    First Commit

---
+++ posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Hi my name is BaneokShin. nice to meet you.
+      </h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
